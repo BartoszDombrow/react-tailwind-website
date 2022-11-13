5e6cca8fbec22db14107116aa78ab9111878e5ba
Bartosz Dombrow <b.dombrow@neurosys.com>
2022-11-14 00:10:00 +0100

    feat: add CardDeal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`bg-blue-gradient py-4 px-6 font-poppins text-[18px] font-medium text-primary outline-none ${styles}`}
+      className={`bg-blue-gradient rounded-[10px] py-4 px-6 font-poppins text-[18px] font-medium text-primary outline-none ${styles}`}
     >
       Get Started
     </button>
