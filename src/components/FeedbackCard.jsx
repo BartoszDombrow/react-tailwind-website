235e3628ae64c66c3a18e3be4dd7cd416e98ff91
Bartosz Dombrow <b.dombrow@neurosys.com>
2022-11-14 00:45:22 +0100

    feat: add Testimonials and FeedbackCard sections

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { quotes } from '../assets'
 
-const FeedbackCard = () => {
+const FeedbackCard = ({ content, name, title, img }) => {
   return (
-    <div>FeedbackCard</div>
+    <div className="justify-betwenn mr-b feedback-card my-5 flex max-w-[370px] flex-col rounded-[20px] px-10 py-12 sm:mr-5 md:mr-10">
+      <img
+        src={quotes}
+        alt="double_quotes"
+        className="h-[72px] w-[42px] object-contain"
+      />
+      <p className="my-10 font-poppins text-[18px] font-normal leading-[32px] text-white">
+        {content}
+      </p>
+      <div className="flex flex-row">
+        <img src={img} alt={name} className="h-[48px] w-[48px] rounded-full" />
+        <div className="ml-4 flex flex-col">
+          <h4 className="font-poppins text-[20px] font-semibold leading-[32px] text-white">
+            {name}
+          </h4>
+          <p className="font-poppins text-[16px] font-normal leading-[24px] text-dimWhite">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
